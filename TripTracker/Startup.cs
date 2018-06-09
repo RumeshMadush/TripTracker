a05a9dd576c4cbb15b47c208350e60b3ef0ccf15
RumeshCrash <rumeshmadushanka95@gmail.com>
2018-06-09 15:07:55 +0530

    intial commit
    basic structure

--- TripTracker/Startup.cs
+++ TripTracker/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace TripTracker
 {
@@ -23,12 +24,27 @@ namespace TripTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<Models.Repository>();
             services.AddMvc();
+            services.AddSwaggerGen(option =>
+            option.SwaggerDoc("v1", new Info { Title = "Trip Tracker ", Version = "v1" })
+            );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSwagger();
+
+            if (env.IsDevelopment() || env.IsStaging())
+            {
+                app.UseSwaggerUI(option =>
+            option.SwaggerEndpoint("/swagger/v1/swagger.json", "Trip Tracker v1")
+            );
+
+            }
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
